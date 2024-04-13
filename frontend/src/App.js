1796f3b0e12aeeee721d581dd87a2e297d10ac73
hriday330 <hriday2004@outlook.com>
2024-04-13 10:30:59 -0700

    Added navbar and made components responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Graph from './components/Graph/Graph';
+import About from './components/About/About';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     // eslint-disable-next-line react/jsx-filename-extension
-    <Graph initialNodes={[{ x: 400, y: 300 }]} initialLinks={[]} width={1500} height={800} />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <Graph
+              initialNodes={[{ x: 400, y: 300 }]}
+              initialLinks={[]}
+              width={1500}
+              height={800}
+            />
+          )}
+        />
+        <Route
+          path="/about"
+          element={(
+            <About />
+          )}
+        />
+        <Route
+          path="/login"
+          element={(
+            <Login />
+          )}
+        />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
